8b26ccbce337ecb54645b2d230aa732570a9cd96
Renan Veronez Drechsler <renanvz@outlook.com>
2023-07-29 10:29:26 +0100

    Input e output do Zod

---
+++ src/utils/convert-time-string-to-minutes.ts
@@ -0,0 +1,6 @@
+export function convertTimeStringToMinutes(timeString: string) {
+  const [hours, minutes] = timeString.split(':').map(Number)
+  // const [hours, minutes] = timeString.split(':').map(item => Number(item))
+
+  return hours * 60 + minutes
+}
